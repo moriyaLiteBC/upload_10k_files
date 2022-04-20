127d7c7a62e0d21a33bf3ecc868b7088957722e5
moriyaLiteBC <moriya@liteBC.tech>
2022-04-20 22:11:26 +0300

    care bugs and if upload failed so continue upload next time

--- upload_10k_files.py
+++ upload_10k_files.py
@@ -16,13 +16,10 @@ import io
 import os
 from typing import TypeVar, Callable
 
-# TODO: if uploaded stuck
-# TODO: check if file exist and then upload according of it.
-# TODO: log for uploaded
-
 T = TypeVar('T')
 _ichor_api_client = None
 _ichor_api_cache = {}
+log_num_lines = 0
 
 
 def load_ichor_configuration():
@@ -45,18 +42,60 @@ def get_ichor_api(api: Callable[[], T]) -> T:
 
 def is_patient_exist(patient_barcode):
     patients = get_ichor_api(PatientsApi).patients_get()
-    patients_barcode = [patient.external_identifier for patient in patients]
-    if patient_barcode in patients_barcode:
+    for patient in patients:
+        if patient.external_identifier == patient_barcode:
+            return patient
+    return None
+
+
+def is_file_exist(file_path, log_path):
+    try:
+        log_file = open(log_path, 'r')
+    except Exception:
+        return None
+    for line in log_file.readlines():
+        splits = line.split(',')
+        if splits[0] == file_path:
+            file_id = int(splits[1])
+            file = get_ichor_api(FilesApi).files_file_id_get(file_id)
+            return file
+    return None
+
+
+def is_record_in_s3(file):
+    import boto3
+    s3 = boto3.resource("s3")
+    key = file.s3_key
+    bucket = file.s3_bucket
+    try:
+        s3.Object(bucket, key).load()
         return True
-    return False
+    except Exception:
+        return False
 
 
-def is_file_exist(file_key):
+def is_record_but_not_in_s3(file_path):
     files = get_ichor_api(FilesApi).files_get()
-    files_paths = [file.original_file_path for file in files]
-    if file_key in files_paths:
-        return True
-    return False
+    for file in files:
+        if file_path == file.original_file_path:
+            if not is_record_in_s3(file):
+                print("record in table but not in s3!\nfile ID: {}\nfile path: {}\n".format(file.file_id, file.original_file_path))
+                return file
+    return None
+
+
+def is_data_instance_exist(data_instance_path, log_path):
+    try:
+        log_file = open(log_path, 'r')
+    except Exception:
+        return None
+    for line in log_file.readlines():
+        if line.startswith(data_instance_path):
+            file_id = int(line.split(',')[1])
+            file = get_ichor_api(FilesApi).files_file_id_get(file_id)
+            data_instance_id = file.parent_data_instance_id
+            data_instance = get_ichor_api(DataInstancesApi).data_instances_data_instance_id_get(data_instance_id)
+            return data_instance
 
 
 def check_classification(file):
@@ -123,75 +162,118 @@ def upload_file(path_file, file_record):
     print('-----------END-----------', '\r\n\r\n')
 
 
-def write_log(log_path, file_name):
+def write_log(log_path, file_path, file_id):
     f = open(log_path, "a")
-    f.write(file_name + "\r\n")
+    f.write(file_path + "," + str(file_id) + "\r")
     f.close()
 
 
-def create_file(subdir, file_name, data_instance_path, data_instance, log_path):
-    file_path = os.path.join(subdir, file_name)
-    # create file in file table.
-    file_key_name = os.path.relpath(os.path.join(subdir, file_name), data_instance_path)
-    if not is_file_exist(file_key_name):
-        last_modified_date = datetime.datetime.strptime(time.ctime(os.path.getmtime(file_path)),
-                                                        "%a %b %d %H:%M:%S %Y")
-        created_date = datetime.datetime.strptime(time.ctime(os.path.getctime(file_path)),
-                                                  "%a %b %d %H:%M:%S %Y")
-        oldest = min([last_modified_date, created_date])
-        classification = check_classification(file_name)
-        file_size = os.path.getsize(file_path)
-        # TODO: user uploaded
-        user_uploaded = 1
-        created_file = File(file_created_date=oldest,
-                            original_file_path=file_key_name,
-                            classification=classification,
-                            parent_data_instance_id=data_instance.data_instance_id,
-                            file_size=file_size,
-                            user_uploaded=user_uploaded)
-        # upload file to file table
-        file = get_ichor_api(FilesApi).files_post(file=created_file)
-        # upload file to Amazon aws
-        upload_file(file_path, file)
-        write_log(log_path, file_path)
+def is_file_in_log(log_path, file_path):
+    with open(log_path) as f:
+        if file_path in f.read():
+            return True
+        return False
+
+
+def get_lines_count_in_file(file_path):
+    try:
+        with open(file_path, 'r') as fp:
+            for count, line in enumerate(fp):
+                pass
+        return count
+    except Exception:
+        return 0
 
 
 def create_patient(patient_dir_path, data_source, log_path):  # C:\Users\user\Desktop\test_upload_file\mesurement1\barcode-patient
     patient_barcode = os.path.basename(patient_dir_path)
-    if not is_patient_exist(patient_barcode):
+    patient = is_patient_exist(patient_barcode)
+    if patient is None:
         patient = get_ichor_api(PatientsApi).patients_post(patient=Patient(external_identifier=patient_barcode))
-        for data_instance_dir in os.listdir(patient_dir_path):
+    for data_instance_dir in os.listdir(patient_dir_path):
+        data_instance_path = os.path.join(patient_dir_path, data_instance_dir)
+        data_instance = is_data_instance_exist(data_instance_path, log_path)
+        if data_instance is None:
             data_instance = get_ichor_api(DataInstancesApi).data_instances_post(
                 data_instance=DataInstance(patient_id=patient.patient_id, data_source=data_source,
-                                           type=''.join([i for i in data_instance_dir.split('_', 1)[0].upper() if not i.isdigit()])))
-            data_instance_path = os.path.join(patient_dir_path, data_instance_dir)
-            for subdir, dirs, files in os.walk(data_instance_path):
-                for file_name in files:
-                    if file_name == "configuration.txt" and os.path.basename(
-                            subdir) == "PreSequence":  # dont upload configuration file in PreSequnce
-                        continue
-                    create_file(subdir, file_name, data_instance_path, data_instance, log_path)
+                                           type=''.join(
+                                               [i for i in data_instance_dir.split('_', 1)[0].upper() if not i.isdigit()])))
 
-#
-# @click.group()
-# def main():
-#     pass
+        for subdir, dirs, files in os.walk(data_instance_path):
+            for file_name in files:
+                if file_name == "configuration.txt" and os.path.basename(
+                        subdir) == "PreSequence":  # dont upload configuration file in PreSequnce
+                    continue
+                file_path = os.path.join(subdir, file_name)
+                # create file in file table.
+                file_key_name = os.path.relpath(os.path.join(subdir, file_name), data_instance_path).replace("\\", "/")
+                file = is_file_exist(file_path, log_path)
+                if file is None:
+                    last_modified_date = datetime.datetime.strptime(time.ctime(os.path.getmtime(file_path)),
+                                                                    "%a %b %d %H:%M:%S %Y")
+                    created_date = datetime.datetime.strptime(time.ctime(os.path.getctime(file_path)),
+                                                              "%a %b %d %H:%M:%S %Y")
+                    oldest = min([last_modified_date, created_date])
+                    classification = check_classification(file_name)
+                    file_size = os.path.getsize(file_path)
+                    # TODO: user uploaded
+                    user_uploaded = 1
+                    created_file = File(file_created_date=oldest,
+                                        original_file_path=file_key_name,
+                                        classification=classification,
+                                        parent_data_instance_id=data_instance.data_instance_id,
+                                        file_size=file_size,
+                                        file_bytes_uploaded=0,
+                                        user_uploaded=user_uploaded)
+                    file = is_record_but_not_in_s3(file_path)
+                    if file is None:
+                        # upload file to file table
+                        file = get_ichor_api(FilesApi).files_post(file=created_file)
+                        get_ichor_api(FilesApi).files_file_id_put(file.file_id, File(original_file_path=file_path))
+                    # upload file to Amazon aws
+                    upload_file(file_path, file)
+                    write_log(log_path, file_path, file.file_id)
+                else:
+                    data_instance = get_ichor_api(DataInstancesApi).data_instances_data_instance_id_get(int(file.parent_data_instance_id))
+                    patient = get_ichor_api(PatientsApi).patients_patient_id_get(data_instance.patient_id)
+                    print("try upload file ID: {}, but its uploaded yet.\n in path: {}\{}\n".format(file.file_id, patient.external_identifier, file.original_file_path))
+
+@click.group()
+def main():
+    pass
 
 
-# @main.command()
-# @click.argument('path')
-# @click.argument('data_source')
+@main.command()
+@click.argument('path')
+@click.argument('data_source')
+@click.argument('destination_path')
 def upload(path, data_source, destination_path=r"C:\Users\user\Desktop\log.txt"):
+    global log_num_lines
+    log_num_lines = get_lines_count_in_file(destination_path)
     load_ichor_configuration()
     for measurement in os.listdir(path):  # iterate over all measurements
         measurement_path = os.path.join(path, measurement)
         for patient_barcode in os.listdir(measurement_path):
             patient_path = os.path.join(measurement_path, patient_barcode)
             create_patient(patient_path, data_source, destination_path)
+    print_done()
 
+
+def print_done():
+    print("          DONE! ")
+    print("      |         |         ")
+    print("      |         |         ")
+    print("      |         |         ")
+    print("|     |         |       |")
+    print("|                       |")
+    print(" \                     / ")
+    print("  \                   / ")
+    print("   \                 / ")
+    print("    \               / ")
+    print("     \_____________/ ")
 
 if __name__ == '__main__':
     # command for upload from path:
     # python ./upload_10k_files.py upload "C:\Users\user\Desktop\test_upload_file" "10K"
-    # main()
-    upload(r"C:\Users\user\Desktop\test_upload_file", "10K")
+    main()
+    # upload(r"C:\Users\user\Desktop\test_upload_file", "10K")
